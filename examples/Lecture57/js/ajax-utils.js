1b2da84d1f30873646afe80e2ae778141f6876a0
MyungJune Shin <kevinshin123@gmail.com>
2022-08-09 14:19:11 +0000

    added some comments to ajax-utils.js

--- examples/Lecture57/js/ajax-utils.js
+++ examples/Lecture57/js/ajax-utils.js
@@ -1,53 +1,42 @@
-(function (global) {
+(function (global) { //IIFE
 
-// Set up a namespace for our utility
-var ajaxUtils = {};
+  // Set up a namespace for our utility
+  var ajaxUtils = {};
 
-
-// Returns an HTTP request object
-function getRequestObject() {
-  if (global.XMLHttpRequest) {
-    return (new XMLHttpRequest());
-  } 
-  else if (global.ActiveXObject) {
-    // For very old IE browsers (optional)
-    return (new ActiveXObject("Microsoft.XMLHTTP"));
-  } 
-  else {
-    global.alert("Ajax is not supported!");
-    return(null); 
+  // not attached to ajaxUtils -> hidden function from world
+  // Returns an HTTP request object
+  function getRequestObject() {
+    if (global.XMLHttpRequest) {
+      return new XMLHttpRequest();
+    } else if (global.ActiveXObject) {
+      // For very old IE browsers (optional)
+      return new ActiveXObject("Microsoft.XMLHTTP");
+    } else {
+      global.alert("Ajax is not supported!");
+      return null;
+    }
   }
-}
 
-
-// Makes an Ajax GET request to 'requestUrl'
-ajaxUtils.sendGetRequest = 
-  function(requestUrl, responseHandler) {
+  // Makes an Ajax GET request to 'requestUrl'
+  ajaxUtils.sendGetRequest = function (requestUrl, responseHandler) {
     var request = getRequestObject();
     request.onreadystatechange = 
-      function() { 
-        handleResponse(request, responseHandler); 
-      };
-    request.open("GET", requestUrl, true);
-    request.send(null); // for POST only
+      function () {
+        handleResponse(request, responseHandler);
+      }; //a function value
+    request.open("GET", requestUrl, true); //true: asynchronous
+    request.send(null); // null: for POST only
   };
 
-
-// Only calls user provided 'responseHandler'
-// function if response is ready
-// and not an error
-function handleResponse(request,
-                        responseHandler) {
-  if ((request.readyState == 4) &&
-     (request.status == 200)) {
-    responseHandler(request);
+  // Only calls user provided 'responseHandler'
+  // function if response is ready
+  // and not an error
+  function handleResponse(request, responseHandler) {
+    if (request.readyState == 4 && request.status == 200) {
+      responseHandler(request);
+    }
   }
-}
 
-
-// Expose utility to the global object
-global.$ajaxUtils = ajaxUtils;
-
-
+  // Expose utility to the global object
+  global.$ajaxUtils = ajaxUtils;
 })(window);
-
